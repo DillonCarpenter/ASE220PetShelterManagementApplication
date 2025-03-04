868a50eb926dd7d58eade0596b435033084e8358
FallingCar <drewbug722@gmail.com>
2025-03-04 10:30:32 -0500

    update display of cards

--- mainPage.js
+++ mainPage.js
@@ -17,28 +17,29 @@ function loadCards(){
     container.innerHTML = '';
     let cardHTML = ''
     jsonData.forEach((pet,index) => {
-        if(index % 3 == 0){
-            cardHTML += '<div class="row">\n'
-        }
-        cardHTML += `
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="${pet.image}" class="card-img-top" alt="Image of the pet">
-                <div class="card-body">
-                    <h5 class="card-title" style="color: ${pet.color};">${pet.name} - ${pet.breed} ${pet.type}</h5>
-                    <p class="card-text">Sex: ${pet.sex} | Age: ${pet.age} | Color: ${pet.color} | Spayed: ${pet.spayed}</p>
-                    <a href="detailLayout.html?id=${pet.ID}" class="btn btn-primary">View detailed page</a>
-                    <a class="btn btn-primary" href="delete.html?id=${pet.ID}">Delete</a>
+        if(index < 9){
+            if(index % 3 == 0){
+                cardHTML += '<div class="row">\n'
+            }
+            cardHTML += `
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="${pet.image}" class="card-img-top" alt="Image of the pet">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color: ${pet.color};">${pet.name} - ${pet.breed} ${pet.type}</h5>
+                        <p class="card-text">Sex: ${pet.sex} | Age: ${pet.age} | Color: ${pet.color} | Spayed: ${pet.spayed}</p>
+                        <a href="detailLayout.html?id=${pet.ID}" class="btn btn-primary">View detailed page</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        `;
-        if(index % 3 == 2){
-            cardHTML += '\n</div>\n'
+            `;
+            if(index % 3 == 2){
+                cardHTML += '\n</div>\n'
+            }
         }
     });
     if (jsonData.length % 3 !== 0) {
-        cardHTML += '</div>\n';
+        cardHTML += '\n</div>\n';
     }
     container.innerHTML += cardHTML;
     //This isn't getting added for some reason.
