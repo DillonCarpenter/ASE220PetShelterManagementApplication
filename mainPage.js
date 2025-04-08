6fb7355c22dad2e87957484ce4f5f2aab681e25c
FallingCar <drewbug722@gmail.com>
2025-04-08 16:20:15 -0400

    applied modal

--- mainPage.js
+++ mainPage.js
@@ -25,7 +25,33 @@ function loadCards(){
                             <div class="card-body">
                                 <h5 class="card-title" style="color: ${pet.color};">${pet.name} - ${pet.breed} ${pet.type}</h5>
                                 <p class="card-text">Sex: ${pet.sex} | Age: ${pet.age} | Color: ${pet.color} | Spayed: ${pet.spayed}</p>
-                                <a href="detailLayout.html?id=${pet.ID}" class="btn btn-primary">View detailed page</a>
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#petModal">
+                                    Show Pet Details
+                                </button>
+
+                                <div class="modal fade" id="petModal" tabindex="-1" aria-labelledby="petModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="petModalLabel">Pet Details</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="row">
+                                                    <article class="col-6" id="info">
+                                                        <p id="pet-info">${pet.name} is a ${pet.age} year old ${pet.sex} ${pet.breed} ${pet.type} waiting for a home. ${pet.description}<br><br> Color: ${pet.color} <br> Spayed: ${pet.spayed} <br> Status: ${pet.status} <br> Microchip Number: ${pet.microchip_number}</p>
+                                                    </article>
+                                                    <aside class="col-6 d-flex justify-content-end align-items-center" id="picture">
+                                                        <img src="${pet.image}" class="img-fluid" alt="Image of pet" id="pet-image">
+                                                    </aside>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
